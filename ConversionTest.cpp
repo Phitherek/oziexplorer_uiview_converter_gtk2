6514ec775c45842586d546ef27064bec5a39f61f
Phitherek_ <phitherek@gmail.com>
2015-11-29 16:22:04 +0100

    Better ConversionTest.

--- ConversionTest.cpp
+++ ConversionTest.cpp
@@ -3,21 +3,27 @@
 //
 #include <iostream>
 #include <cstdlib>
+#include <typeinfo>
 #include "Converter.h"
 #include "ConversionError.h"
 
 using namespace std;
 
 int main() {
-    cout << "ConversionTest for OziExplorer to UI-View Converter v. 0.2 (C) 2015 by Phitherek_ SO9PH" << std::endl;
     try {
-        Converter c("siatka.map", "siatka.inf");
-        c.convert();
-    } catch(ConversionError& e) {
-        cerr << "Caught ConversionError: " << e.what() << std::endl;
-        cout << "Failure!" << std::endl;
+        cout << "ConversionTest for OziExplorer to UI-View Converter v. 0.2 (C) 2015 by Phitherek_ SO9PH" << std::endl;
+        try {
+            Converter c("siatka.map", "siatka.inf");
+            c.convert();
+        } catch(ConversionError& e) {
+            cerr << "Caught ConversionError: " << e.what() << std::endl;
+            cout << "Failure!" << std::endl;
+            return EXIT_FAILURE;
+        }
+        cout << "Success!" << std::endl;
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        cout << "Caught exception: " << typeid(e).name() << ": " << e.what() << endl;
         return EXIT_FAILURE;
     }
-    cout << "Success!" << std::endl;
-    return EXIT_SUCCESS;
 }
