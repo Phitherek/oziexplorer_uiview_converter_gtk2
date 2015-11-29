d61a0a25a1a1a1fd9911ac9f1c72965604ba944a
Phitherek_ <phitherek@gmail.com>
2015-11-29 15:26:57 +0100

    Custom endline handler added to ConversionTest output.

--- ConversionTest.cpp
+++ ConversionTest.cpp
@@ -5,19 +5,20 @@
 #include <cstdlib>
 #include "Converter.h"
 #include "ConversionError.h"
+#include "Helper.h"
 
 using namespace std;
 
 int main() {
-    cout << "ConversionTest for OziExplorer to UI-View Converter v. 0.2 (C) 2015 by Phitherek_ SO9PH" << endl;
+    cout << "ConversionTest for OziExplorer to UI-View Converter v. 0.2 (C) 2015 by Phitherek_ SO9PH" << Helper::endline();
     try {
         Converter c("siatka.map", "siatka.inf");
         c.convert();
     } catch(ConversionError& e) {
-        cerr << "Caught ConversionError: " << e.what() << endl;
-        cout << "Failure!" << endl;
+        cerr << "Caught ConversionError: " << e.what() << Helper::endline();
+        cout << "Failure!" << Helper::endline();
         return EXIT_FAILURE;
     }
-    cout << "Success!" << endl;
+    cout << "Success!" << Helper::endline();
     return EXIT_SUCCESS;
 }
